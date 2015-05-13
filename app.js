25007e4646c63ae48777f7412005127d951e8a24
Tremayne <trey@Trey-MBP.local>
2015-05-13 12:56:43 -0400

    added bower

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/bower_components', express.static(path.join(__dirname, '/bower_components')));
 
 
 app.use('/', routes);
